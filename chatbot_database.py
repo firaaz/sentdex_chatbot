558924d9d0ad53b9ec1ca589d7b209ba84f84413
Firaaz <firaazfarook19@gmail.com>
2019-11-03 07:39:54 +0530

    initial commit

---
+++ chatbot_database.py
@@ -0,0 +1,54 @@
+"""
+Module related to the database management
+"""
+import sqlite3
+import json
+from datetime import datetime
+
+timeframe = '2015-05'
+sql_transaction = []
+
+CONNECTION = sqlite3.connect('{}.db'.format(timeframe))
+CURSOR = CONNECTION.cursor()
+
+
+def create_table():
+    """
+    Creates a table if it does not already exist
+    """
+    CURSOR.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id parent_id TEXT PRIMARY KEY, comment_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+
+def format_data(data):
+    """
+    Does the clean ups required
+    :return : formatted data
+    """
+    data = data.replace("\n", " newlinechar ").replace("\r", " returnchar ").replace('"', "'")
+    return data
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment from parent_reply where comment_id = '{}' LIMIT 1".format(pid)
+        CURSOR.execute(sql)
+        result = CURSOR.fetchone()
+        if result is not None:
+            return result[0]
+        return False
+    except Exception as e:
+        print("find_parent", e)
+        return False
+
+if __name__ == '__main__':
+    create_table()
+    row_counter = 0
+    paired_rows = 0
+    with open('~/projects/reddit_chat_data/RC_2015-01', buffer=1000) as f:
+        for row in f:
+            row_counter += 1
+            row = json.load(row)
+            parent_id = row['parent_id']
+            body = row['body']
+            score = row['score']
+            subreddit = row['subreddit']
+
+            parent_data = find_parent(parent_id)
